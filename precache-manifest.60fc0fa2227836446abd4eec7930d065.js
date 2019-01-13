1d202e706dda809e7d0149bbebe438855b50d853
David Bain <davidbaindesign@gmail.com>
2019-01-13 18:59:09 -0500

    Add files via upload

---
+++ precache-manifest.60fc0fa2227836446abd4eec7930d065.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f5388fac608dfe77f4343d38abf122f",
+    "url": "./static/media/fontawesome-webfont.9f5388fa.ttf"
+  },
+  {
+    "revision": "67384322d4e923352337",
+    "url": "./static/css/main.e863a130.chunk.css"
+  },
+  {
+    "revision": "6b09db63b5a5859b9ca4",
+    "url": "./static/js/1.6b09db63.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "e5f1ea61ee87f0754d76953cfdff2d57",
+    "url": "./static/media/baindesign.e5f1ea61.png"
+  },
+  {
+    "revision": "a1a450ea331a3fc89e867ecad963b6b2",
+    "url": "./static/media/fontawesome-webfont.a1a450ea.woff2"
+  },
+  {
+    "revision": "a247f4358b253516c32943603c4c5970",
+    "url": "./static/media/fontawesome-webfont.a247f435.eot"
+  },
+  {
+    "revision": "f84ffa8dd9706d3cf7c8e800372c0cc0",
+    "url": "./static/media/fontawesome-webfont.f84ffa8d.woff"
+  },
+  {
+    "revision": "67384322d4e923352337",
+    "url": "./static/js/main.67384322.chunk.js"
+  },
+  {
+    "revision": "6c4c3504e3402b6f1b75332b796da253",
+    "url": "./static/media/fontawesome-webfont.6c4c3504.svg"
+  },
+  {
+    "revision": "4634449c3e8c76f0d32d5fc6ed961aa6",
+    "url": "./static/media/baindesign.4634449c.jpg"
+  },
+  {
+    "revision": "7574ee735c1f9dbd5916b3bb82bec3bf",
+    "url": "./static/media/calculator.7574ee73.jpg"
+  },
+  {
+    "revision": "69272070a9c64236a0e087fddf7e1b45",
+    "url": "./static/media/nintendo.69272070.jpg"
+  },
+  {
+    "revision": "5f78347b1074fcb8917ac6cec9729640",
+    "url": "./static/media/pomodoro.5f78347b.jpg"
+  },
+  {
+    "revision": "f304b354c39b97fca914054707ff46e3",
+    "url": "./static/media/weather.f304b354.jpg"
+  },
+  {
+    "revision": "e898680ffe6816472e8812cd40905530",
+    "url": "./index.html"
+  }
+];
