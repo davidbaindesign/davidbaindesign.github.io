4e648dab1d35e486d6ecb66de31241d636895e43
David Bain <davidbaindesign@gmail.com>
2020-01-22 15:01:31 -0500

    Add files via upload
    New build

---
+++ precache-manifest.542dc569b87bae92b0030de84da7f88f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1cb3a230dfafcb4329cab934e3ea566",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d4d3ba4ab4fa864b985c",
+    "url": "./static/css/main.df4e2a5f.chunk.css"
+  },
+  {
+    "revision": "4b7f4e06375d5b4e12b5",
+    "url": "./static/js/2.8ff1aa3a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.8ff1aa3a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d4d3ba4ab4fa864b985c",
+    "url": "./static/js/main.cf72a49d.chunk.js"
+  },
+  {
+    "revision": "567ca5cea940a0c54188",
+    "url": "./static/js/runtime-main.1a632c21.js"
+  },
+  {
+    "revision": "4634449c3e8c76f0d32d5fc6ed961aa6",
+    "url": "./static/media/baindesign.4634449c.jpg"
+  },
+  {
+    "revision": "e5f1ea61ee87f0754d76953cfdff2d57",
+    "url": "./static/media/baindesign.e5f1ea61.png"
+  },
+  {
+    "revision": "7574ee735c1f9dbd5916b3bb82bec3bf",
+    "url": "./static/media/calculator.7574ee73.jpg"
+  },
+  {
+    "revision": "5de86af42e60ad1e55516fd326ae88a6",
+    "url": "./static/media/fontawesome-webfont.5de86af4.svg"
+  },
+  {
+    "revision": "9f5388fac608dfe77f4343d38abf122f",
+    "url": "./static/media/fontawesome-webfont.9f5388fa.ttf"
+  },
+  {
+    "revision": "a1a450ea331a3fc89e867ecad963b6b2",
+    "url": "./static/media/fontawesome-webfont.a1a450ea.woff2"
+  },
+  {
+    "revision": "a247f4358b253516c32943603c4c5970",
+    "url": "./static/media/fontawesome-webfont.a247f435.eot"
+  },
+  {
+    "revision": "f84ffa8dd9706d3cf7c8e800372c0cc0",
+    "url": "./static/media/fontawesome-webfont.f84ffa8d.woff"
+  },
+  {
+    "revision": "69272070a9c64236a0e087fddf7e1b45",
+    "url": "./static/media/nintendo.69272070.jpg"
+  },
+  {
+    "revision": "5f78347b1074fcb8917ac6cec9729640",
+    "url": "./static/media/pomodoro.5f78347b.jpg"
+  },
+  {
+    "revision": "f304b354c39b97fca914054707ff46e3",
+    "url": "./static/media/weather.f304b354.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.60fc0fa2227836446abd4eec7930d065.js"
+  "./precache-manifest.542dc569b87bae92b0030de84da7f88f.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
